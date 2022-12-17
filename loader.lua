34421849ea5b358e921299b4c0339fd36ec81af8
Peem 3 <94160982+peemx1@users.noreply.github.com>
2022-12-17 22:22:56 +0700

    Delete loader.lua

--- loader.lua
+++
@@ -1,4 +0,0 @@
-if game.PlaceId 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/peemx1/log-description/main/wtf.lua"))()
-else
-    game.Players:WaitForChild("LocalPlayer"):Kick("\nThis game not support.")
