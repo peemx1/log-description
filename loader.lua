3e2e48b4ec57fb994211d798fb4ed6bc4fcb1e50
Peem 3 <94160982+peemx1@users.noreply.github.com>
2022-12-17 22:06:57 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,3 @@
-shared.port = 7963
 if game.PlaceId 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/peemx1/log-description/main/wtf.lua"))()
 else
